12313503e20906b358f3b0758e360d2608e43f1e
hanaarf <arifhana886@gmail.com>
2024-09-21 13:15:17 +0700

    deleteForeign

--- hapus.php
+++ hapus.php
@@ -1,7 +1,8 @@
 <?php
 include './koneksi.php';
 $id = $_GET['id'];
-
+mysqli_query($koneksi, "SET FOREIGN_KEY_CHECKS=0");
 mysqli_query($koneksi, "DELETE FROM peserta where id='$id'");
+mysqli_query($koneksi, "SET FOREIGN_KEY_CHECKS=1");
 header("location:index.php");
 ?>
